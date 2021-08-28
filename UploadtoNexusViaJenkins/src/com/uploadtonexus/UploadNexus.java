65178bebfb291437c1b82a8b0d45cfd759cfa149
forough <forough.safari1986@gmail.com>
2021-08-28 09:22:08 +0430

    first commit

---
+++ UploadtoNexusViaJenkins/src/com/uploadtonexus/UploadNexus.java
@@ -0,0 +1,10 @@
+package com.uploadtonexus;
+
+public class UploadNexus {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Upload completed successfully");
+	}
+
+}
